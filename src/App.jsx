0da3d15a089a4da10c7a620e928b0391d81a92e4
guskumite <gmolanoruiz2000@gmail.com>
2023-08-23 12:36:51 -0500

    version 1.0.5

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,18 @@ function App() {
       picture:
         "https://m.media-amazon.com/images/I/41oihh5M3jL._SX331_BO1,204,203,200_.jpg",
     },
+    {
+      id: 3,
+      url: "https://bodegacool.myshopify.com",
+      picture:
+        "https://app.prodia.com/art-ai?gclid=Cj0KCQjwoK2mBhDzARIsADGbjepkM3ecI_hSoCtYdTwcC2_kqCgBBftW83UkMvd87JKzrHkASAdJB6caAggnEALw_wcB",
+    },
+    {
+      id: 4,
+      url: "https://amzn.to/3YMhjaQ",
+      picture:
+        "https://m.media-amazon.com/images/I/51Av8lN8yiL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+    },
   ];
   return (
     <main>
