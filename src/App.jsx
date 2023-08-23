ff027c67a122314632d707acbe8e63d315d70d57
guskumite <gmolanoruiz2000@gmail.com>
2023-08-23 00:50:22 -0500

    version 1.0.1

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,14 @@ function App() {
   const mybooks = [
     {
       id: 1,
-      url: "https://www.amazon.com/dp/B0B7R4Q5DJ/ref=s9_acsd_al_bw_c2_x_0_i?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-4&pf_rd_r=0V02RZY7A0C255QH3D5K&pf_rd_t=101&pf_rd_p=44155fb9-6bb0-4c59-ac88-727ad0c4d569&pf_rd_i=3003015011",
+      url: "https://amzn.to/45zja5D",
       picture: "https://m.media-amazon.com/images/I/51YGx8qVVXL.jpg",
+    },
+    {
+      id: 2,
+      url: "https://amzn.to/44kImLB",
+      picture:
+        "https://m.media-amazon.com/images/I/41oihh5M3jL._SX331_BO1,204,203,200_.jpg",
     },
   ];
   return (
--- src/components/Booklist.jsx
+++ src/components/Booklist.jsx
@@ -4,27 +4,28 @@ const Booklist = ({ mybooks }) => {
   };
 
   return (
-    <section>
-      <ul className="space-y-4 w-[40rem]">
-        {mybooks.map((mybook) => {
-          return (
-            <li className="flex flex-col items-center" key={mybook.id}>
-              <img
-                className="w-[40%] h-[60%]"
-                src={mybook.picture}
-                alt={mybook.title}
-              />
-              <button
-                className="mb-2 mt-2 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded"
-                onClick={() => handleBuy(mybook.url)}
-              >
-                Buy Now
-              </button>
-            </li>
-          );
-        })}
-      </ul>
-    </section>
+      <section>
+        <ul className="space-y-4 w-[40rem]">
+          {mybooks.map((mybook) => {
+            return (
+              <li className="flex flex-col items-center" key={mybook.id}>
+                <img
+                  className="w-[40%] h-[60%]"
+                  src={mybook.picture}
+                  alt={mybook.title}
+                />
+                <button
+                  className="mb-2 mt-2 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded"
+                  onClick={() => handleBuy(mybook.url)}
+                >
+                  Buy Now
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+    </article>
   );
 };
 
