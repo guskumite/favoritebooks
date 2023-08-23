a412984ae0f17fa96c564138fb47b92af34e6208
guskumite <gmolanoruiz2000@gmail.com>
2023-08-23 18:58:32 -0500

    version 1.0.7

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,66 @@ import "./App.css";
 import Booklist from "./components/Booklist";
 
 function App() {
-  const mybooks = [
+  const otherStuff = [
     {
       id: 1,
-      url: "https://amzn.to/45zja5D",
-      picture: "https://m.media-amazon.com/images/I/51YGx8qVVXL.jpg",
-    },
-    {
-      id: 2,
-      url: "https://amzn.to/44kImLB",
-      picture:
-        "https://m.media-amazon.com/images/I/41oihh5M3jL._SX331_BO1,204,203,200_.jpg",
-    },
-    {
-      id: 3,
+      title: "Bodega cool: Visit my Store in shopify!",
       url: "https://bodegacool.myshopify.com",
+      btntitle: "Buy Now",
       picture:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTZe20pkN0Xh4aRAKjgN3BH_VP9_7lwN7NJg&usqp=CAU",
     },
     {
-      id: 4,
+      id: 2,
+      title: "Amazon Basics Neoprene Workout Dumbbell",
+      url: "https://amzn.to/3OQwlbb",
+      btntitle: "Buy Now",
+      picture:
+        "https://images-na.ssl-images-amazon.com/images/I/61oSwPvRWLL._AC_UL225_SR225,160_.jpg",
+    },
+  ];
+  const mybooks = [
+    {
+      id: 1,
+      title: "Amazon books: Hello Beautiful",
+      url: "https://amzn.to/45zja5D",
+      btntitle: "Buy Now",
+      picture: "https://m.media-amazon.com/images/I/51YGx8qVVXL.jpg",
+    },
+    {
+      id: 2,
+      title: "Amazon books: Necessary Trouble",
+      url: "https://amzn.to/44kImLB",
+      btntitle: "Buy Now",
+      picture:
+        "https://m.media-amazon.com/images/I/41oihh5M3jL._SX331_BO1,204,203,200_.jpg",
+    },
+
+    {
+      id: 3,
+      title: "Amazon books: Spanish Edition: Shintergy, La puerta de atrás",
       url: "https://amzn.to/3YMhjaQ",
+      btntitle: "Buy Now",
       picture:
         "https://m.media-amazon.com/images/I/51Av8lN8yiL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
     },
   ];
+
+  mybooks.push({
+    id: 4,
+    title: "Free books in Spanish, libros esotéricos en español",
+    url: "https://www.libroesoterico.com",
+    btntitle: "Visit-Visitar",
+    picture:
+      "https://libroesoterico.com/wp-content/webp-express/webp-images/uploads/2021/01/homeimg1-min-300x300.png.webp",
+  });
+
   return (
     <main>
+      <h1 className="text-xl font-extrabold">Books:</h1>
       <Booklist mybooks={mybooks} />
+      <h1 className="text-xl font-extrabold">Miscellaneus:</h1>
+      <Booklist mybooks={otherStuff} />
     </main>
   );
 }
--- src/components/Booklist.jsx
+++ src/components/Booklist.jsx
@@ -8,7 +8,8 @@ const Booklist = ({ mybooks }) => {
       <ul className="space-y-4 flex flex-wrap items-stretch">
         {mybooks.map((mybook) => {
           return (
-            <li className="flex flex-col mt-0" key={mybook.id}>
+            <li className="flex flex-col mt-0 w-96" key={mybook.id}>
+              <h1 className="text-sm">{mybook.title}</h1>
               <img
                 className="ml-2 block h-full"
                 src={mybook.picture}
@@ -18,7 +19,7 @@ const Booklist = ({ mybooks }) => {
                 className="mb-2 mt-2 h-[4rem] w-[7rem] bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded"
                 onClick={() => handleBuy(mybook.url)}
               >
-                Buy Now
+                {mybook.btntitle}
               </button>
             </li>
           );
